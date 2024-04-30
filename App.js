07ef7ff6a8997059a23f5b07ba66f577b8cf4ea3
cedriq jimenez <qcmojimenez@tip.edu.ph>
2024-04-30 22:34:31 +0800

    Push Notif

--- App.js
+++ App.js
@@ -1,6 +1,53 @@
-import React from "react";
+import React, { useEffect } from "react";
+import * as Notifications from 'expo-notifications';
 import Navigation from "./screens/Navigation";
 
 export default function App() {
-  return <Navigation/>;
-};
+  useEffect(() => {
+    registerForPushNotificationsAsync();
+
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Don't forget!",
+        body: "Have you checked your bills yet?",
+      },
+      trigger: {
+        hour: 8,
+        minute: 0,
+        repeats: true,
+      },
+    });
+
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Don't forget!",
+        body: "Have you checked your bills yet?",
+      },
+      trigger: {
+        hour: 20,
+        minute: 0,
+        repeats: true,
+      },
+    });
+
+    return () => {
+    };
+  }, []);
+
+  const registerForPushNotificationsAsync = async () => {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      console.log('Failed to get push token for push notification!');
+      return;
+    }
+    const token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  };
+
+  return <Navigation />;
+}
