8a57f5d54374c4e5bb98edaccb511e93aea7c10f
CABARIO <qbbcabario@tip.edu.ph>
2024-05-10 20:15:43 +0800

    new styles

--- App.js
+++ App.js
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navigation from "./screens/Navigation";
+import { getAllBills } from "./Storage";
 
 export default function App() {
-  return <Navigation/>;
-};
+  useEffect(() => {
+    checkUpcomingBills();
+  }, []);
+
+  const checkUpcomingBills = async () => {
+    try {
+      const parsedBills = await getAllBills();
+      const today = new Date();
+      const sevenDaysFromNow = new Date(today);
+      sevenDaysFromNow.setDate(today.getDate() + 7);
+
+      const upcomingBills = parsedBills.filter(
+        (bill) => new Date(bill.dueDate) <= sevenDaysFromNow
+      );
+
+      if (upcomingBills.length > 0) {
+        const message =
+          "You have bills due within the next 7 days. Check your bills!";
+        alert(message);
+      }
+    } catch (error) {
+      console.error("Error checking upcoming bills:", error);
+    }
+  };
+
+  return <Navigation />;
+}
